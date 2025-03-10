0f44bc912c36aa59831694079fb7d3054adb7468
raiasim93 <asimrai89@gmail.com>
2025-03-10 12:23:09 +1100

    practice server and client components

---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/modules/pages/partials/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Navbar />
+  )
+}
+
+export default page
---
+++ src/components/modules/pages/partials/Navbar.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <button className='flex w-screen justify-center min-h-screen items-center'>
+        Navigation
+    </button>
+  )
+}
+
+export default Navbar
