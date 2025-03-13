0129f9019020113a8cf957822ab5874f47db1bf4
raiasim93 <asimrai89@gmail.com>
2025-03-13 23:38:58 +1100

    Style sidebarOpen and work on routing features when done

--- src/components/partials/Sidebar.tsx
+++ src/components/partials/Sidebar.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { IoClose } from 'react-icons/io5';
 
-const Sidebar = ({setSidebarOpen}: {setSidebarOpen: (open: boolean) => void }) => {
+const Sidebar = ({ setSidebarOpen }: { setSidebarOpen: (open: boolean) => void }) => {
   return (
-   <div className=' py-8 bg-zinc-900'>
-        <IoClose className='text-4xl absolute top-6 right-14 hover:bg-zinc-950 duration-300 ease-in-out rounded-full cursor-pointer '
-                 onClick={()=> setSidebarOpen(false)} />
-        <div className='flex flex-col gap-6 text-center text-xl'>
-        <a href="" className="uppercase hover:text-zinc-500 hover:scale-105 transition-all duration-200">About</a>
-        <a href="" className="uppercase hover:text-zinc-500 hover:scale-105 transition-all duration-200">Work</a>
-        <a href="" className="uppercase hover:text-zinc-500 hover:scale-105 transition-all duration-200">Services</a>
-        <a href="" className="uppercase hover:text-zinc-500 hover:scale-105 transition-all duration-200">Contact</a>
-
-        </div>
-   </div>
-  )
-}
+    <div className="py-8 flex flex-col items-center">
+      <IoClose
+        className="text-4xl absolute top-6 right-14 hover:bg-zinc-950 duration-300 ease-in-out rounded-full cursor-pointer"
+        onClick={() => setSidebarOpen(false)}
+      />
+      <div className="flex flex-col gap-6 text-xl text-center">
+        {["About", "Work", "Services", "Contact"].map((item) => (
+          <a
+            key={item}
+            href=""
+            className="uppercase relative hover:text-zinc-500 hover:scale-105 transition-all duration-200 border-b border-white pb-2 inline-block"
+          >
+            {item}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
