a87c93cdaba1d39d164eca95d5d376abd9c6c3c6
raiasim93 <asimrai89@gmail.com>
2025-03-18 21:16:56 +1100

    Finalise the hero section, leave routing for later and start next section

--- src/components/modules/landing/HeroSection.tsx
+++ src/components/modules/landing/HeroSection.tsx
@@ -5,27 +5,31 @@ import { FaLinkedin, FaGithub, FaFacebook, FaHandPeace } from 'react-icons/fa';
 
 const HeroSection = () => {
     return (
-        <section className='md:h-[calc(100vh-80px)]  w-full py-8 px-[12vw] 2xl:px-[16vw] flex flex-col md:flex-row justify-center items-center bg-neutral-950 text-4xl  '>
+        <section className='min-h-screen  w-full py-8 px-[12vw] 2xl:px-[16vw] flex flex-col gap-x-8 md:flex-row justify-center items-center bg-neutral-950 text-4xl  '>
             {/* Text section */}
-            <div className='w-full mb-12 md:mb-0 md:w-1/2 flex flex-col md:flex-col  md:gap-y-4 text-center md:text-left '>
-                <div className='text-xl flex items-center justify-center md:justify-start md:text-4xl mb-4 md:mb-0 tracking-widest sm:mb-0  w-full'>
-                    Heyy! I'm Asim. <FaHandPeace className='md:text-4xl' />
+            <div className='w-full mb-12 md:mb-0 md:w-1/2 flex flex-col md:flex-col gap-y-2  md:gap-y-12 text-center md:text-left '>
+                <div className='text-xl flex items-center justify-center md:justify-start md:text-2xl mb-4 md:mb-0 tracking-widest sm:mb-0  w-full'>
+                    Heyy! I'm Asim. <FaHandPeace className='text-2xl' />
                 </div>
-                <div className='w-full text-4xl md:text-5xl  tracking-widest leading-12' >
-                    Building Brands, <span className='text-lime-500'> Developing Applications </span>  and Delivering 
+                <div className='w-full text-2xl md:text-4xl  tracking-widest leading-10 md:leading-12 ' >
+                    Building Brands, <span className='text-lime-500'> Developing Applications </span>  and Delivering
                     <span className='text-lime-500'> Great UI/UX </span>
                 </div>
-                <div className='w-full flex flex-col gap-y-8 md:flex-row md:gap-y-0 items-center justify-between mt-16'>
-                    <button className='flex items-center gap-x-2 bg-neutral-600 border border-white text-white px-6 py-2 rounded-full text-xl
-                                        transition-all duration-200 hover:scale-110 hover:border-2 hover:border-white hover:cursor-pointer    
-                                        '>
+
+                <div className='w-full flex flex-wrap gap-y-6 justify-center md:justify-between items-center'>
+                    {/* Explore More Button */}
+                    <div className='group flex items-center bg-neutral-900 border border-white text-white px-6 py-2 rounded-full 
+                        transition-all duration-200 hover:bg-lime-500 hover:scale-110 hover:border-2 hover:border-white hover:cursor-pointer
+                        text-lg md:text-2xl whitespace-nowrap'>
                         Explore More
-                        <BiRightArrowAlt className='text-2xl md:text-4xl transition-all duration-200 hover:translate-x-1.5 hover:scale-110' />
-                    </button>
-                    <div className='flex gap-x-4'>
-                        <FaLinkedin className=' transition-all duration-200 hover:scale-110 hover:border-2  hover:border-white  hover:cursor-pointer' />
-                        <FaGithub className=' transition-all duration-200 hover:scale-110 hover:border-2 hover:border-white hover:cursor-pointer' />
-                        <FaFacebook className=' transition-all duration-200 hover:scale-110 hover:border-2 hover:border-white hover:cursor-pointer' />
+                        <BiRightArrowAlt className='text-2xl md:text-4xl transition-all duration-200 group-hover:translate-x-1.5 ' />
+                    </div>
+
+                    {/* Social Icons */}
+                    <div className='flex gap-x-4 justify-center w-full md:w-auto'>
+                        <FaLinkedin className='transition-all duration-200 hover:scale-110 hover:border-2 hover:border-white hover:cursor-pointer' />
+                        <FaGithub className='transition-all duration-200 hover:scale-110 hover:border-2 hover:border-white hover:cursor-pointer' />
+                        <FaFacebook className='transition-all duration-200 hover:scale-110 hover:border-2 hover:border-white hover:cursor-pointer' />
                     </div>
                 </div>
 
@@ -38,7 +42,7 @@ const HeroSection = () => {
                     alt='Image for hero section'
                     width={120}
                     height={120}
-                    className='aspect-square object-cover rounded-4xl md:rounded-full border-4 md:border-8 border-white w-4/5 md:w-3/4'
+                    className='aspect-square object-cover rounded-4xl md:rounded-full border-4 md:border-6 border-white w-2/3 md:w-[80%]'
                 />
             </div>
 
--- src/components/partials/Sidebar.tsx
+++ src/components/partials/Sidebar.tsx
@@ -1,32 +1,45 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { IoClose } from 'react-icons/io5';
-const Sidebar = ({setSidebarOpen}: {setSidebarOpen: (open: boolean) => void }) => {
+
+const Sidebar = ({ setSidebarOpen }: { setSidebarOpen: (open: boolean) => void }) => {
   
-  useEffect(()=> {
-    const mediaQuery = window.matchMedia("(min-width: 768px");
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 768px)");
     const handleResize = (event: MediaQueryListEvent) => {
-      if(event.matches) setSidebarOpen(false);
+      if (event.matches) setSidebarOpen(false);
     };
     mediaQuery.addEventListener("change", handleResize);
-    return ()=> mediaQuery.removeEventListener("change", handleResize);
+    return () => mediaQuery.removeEventListener("change", handleResize);
   }, [setSidebarOpen]);
+
   return (
-   <div className="py-8 flex flex-col items-center bg-neutral-900 h-screen fixed top-0 left-0 w-full z-50">
-        <IoClose className='text-xl absolute top-6 right-14  hover:bg-zinc-800 rounded-full cursor-pointer '
-                 onClick={()=> setSidebarOpen(false)} />
-        <div className='flex flex-col gap-6 text-center text-lg'>
-          {["About", "Work", "Services", "Contact"].map((item,index)=>(
+    <div className="fixed inset-0 z-50 flex">
+      {/* Background Overlay */}
+      <div 
+        className="fixed inset-0 bg-black opacity-90 transition-opacity duration-300" 
+        onClick={() => setSidebarOpen(false)}
+      />
+
+      {/* Sidebar */}
+      <div className="py-8 flex flex-col items-center bg-neutral-900 h-screen fixed top-0 left-0 w-1/2 z-50">
+        <div className='w-full flex justify-end px-8'>
+        <IoClose className='text-4xl  hover:bg-zinc-800 rounded-full cursor-pointer'
+                 onClick={() => setSidebarOpen(false)} />
+        </div>
+        
+        <div className='flex flex-col gap-6 text-center text-xl'>
+          {["About", "Work", "Services", "Contact"].map((item, index) => (
             <a
               key={index}
               href=''
-              className='uppercase relative hover:text-zinc-400 hover:scale-105 transition-all duration-200 border-b border-white pb-2 inline-block' >
+              className=' relative hover:text-zinc-400 hover:scale-105 transition-all duration-200 border-b border-white pb-2' >
                 {item}
             </a>
           ))}
         </div>
-   </div>
-  )
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
-
