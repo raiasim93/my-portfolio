9ee05d5c8798e510840a7fb2fde4a886eeb78f56
raiasim93 <asimrai89@gmail.com>
2025-04-02 23:05:13 +1100

    remove unsued useRef

--- src/app/about/QualificationSection.tsx
+++ src/app/about/QualificationSection.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import CardQualification from '@/components/common/cards/CardQualification';
 import qualificationData from '@/components/common/data/qualificationData';
 import gsap from 'gsap';
@@ -7,9 +7,6 @@ import scrolltrigger from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(scrolltrigger)
 const QualificationSection = () => {
-  useEffect(()=> {
-
-  }, []);
   return (
     <section className='py-24 px-[12vw] 2xl:[16vw] flex flex-col gap-y-16 justify-center items-center w-full min-h-screen '>
       <div className='text-2xl md:text-5xl'>
