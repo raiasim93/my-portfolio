71febccc3e3616977e15b0ffc847338162a1c036
raiasim93 <asimrai89@gmail.com>
2025-04-04 10:52:00 +1100

    rework on animation of about section

--- src/app/about/AboutSection.tsx
+++ src/app/about/AboutSection.tsx
@@ -12,39 +12,41 @@ const AboutSection = () => {
   const aboutIntroRef = useRef(null);
   const aboutImageRef = useRef(null);
   const aboutOutroRef = useRef(null);
-
-  useEffect(() => {
+  
+  useEffect(()=> {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: sectionRef.current,
-        start: "top 80%",
+        start: "top 60%",
+        end: "bottom 20%",
         toggleActions: "play none none reset",
+        scrub: 1,
+        markers: true,
+        
       }
     });
-  
     tl.fromTo(
       aboutIntroRef.current,
-      { opacity: 0, y: 50 },
-      { opacity: 1, y: 0, duration: 1.5, ease: "power2.out" }
+      {x: 50, opacity: 0},
+      {x: 0, opacity: 1, duration: 1.2, ease:"power1.out"}
     )
-    .fromTo(
+    tl.fromTo(
       aboutImageRef.current,
-      { opacity: 0, scale: 0.8 },
-      { opacity: 1, scale: 1, duration: 1.2, ease: "power2.out" },
+      {x: 100, opacity: 0},
+      {x: 0, opacity: 1, duration: 1.4, ease:"power1.out"},
       "-=1"
     )
-    .fromTo(
+    tl.fromTo(
       aboutOutroRef.current,
-      { opacity: 0, y: -50 },
-      { opacity: 1, y: 0, duration: 1, ease: "power2.out" },
+      {x:100, opacity: 0},
+      {x:0, opacity: 1, duration: 1.6, ease: "power1.out"},
       "-=0.8"
-    );
+    )
   }, []);
-  
 
   return (
     <section ref={sectionRef} className='py-12 px-[12vw] 2xl:px-[16vw] w-full flex flex-col md:flex-row min-h-[100vh] justify-center gap-y-16 md:gap-y-0'>
-      <div ref={aboutIntroRef} className='w-full md:w-1/3 flex flex-col gap-y-4 md:gap-y-8 justify-center md:justify-start opacity-0'>
+      <div ref={aboutIntroRef} className='w-full md:w-1/3 flex flex-col gap-y-4 md:gap-y-8 justify-center md:justify-start '>
         <div className='text-3xl md:text-6xl font-semibold text-center md:text-start'> Hey! I&apos;m <span className='text-lime-400'> Asim. </span> </div>
         <div className='text-lg md:text-xl font-light text-center md:text-start'>
           I&apos;m a Sydney-based Frontend Developer who loves designing and developing impactful brands as well as
@@ -52,7 +54,7 @@ const AboutSection = () => {
         </div>
       </div>
       
-      <div ref={aboutImageRef} className='w-full md:w-1/3 flex justify-center items-center opacity-0'>
+      <div ref={aboutImageRef} className='w-full md:w-1/3 flex justify-center items-center '>
         <Image
           className='aspect-square w-1/2 md:w-2/3 object-cover border-2 border-white rounded-2xl'
           src='/aboutHero.jpeg'
@@ -62,7 +64,7 @@ const AboutSection = () => {
         />
       </div>
       
-      <div ref={aboutOutroRef} className='w-full md:w-1/3 flex flex-col items-center gap-y-4 md:gap-y-2 md:justify-end md:items-end opacity-0'>
+      <div ref={aboutOutroRef} className='w-full md:w-1/3 flex flex-col items-center gap-y-4 md:gap-y-2 md:justify-end md:items-end '>
         <div className='text-2xl md:text-4xl text-end'> Want to know more? </div>
         <div className='text-xl md:text-2xl flex gap-x-4 items-end'> Scroll Below <FaMouse className='text-lime-400' /> </div>
       </div>
