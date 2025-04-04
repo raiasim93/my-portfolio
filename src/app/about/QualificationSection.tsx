7cfd66b0c9f4c90549780b178367cfba20c18739
raiasim93 <asimrai89@gmail.com>
2025-04-04 20:25:34 +1100

    Finish animation in the card component

--- src/app/about/QualificationSection.tsx
+++ src/app/about/QualificationSection.tsx
@@ -9,23 +9,33 @@ gsap.registerPlugin(scrolltrigger)
 const QualificationSection = () => {
   const qualificationSectionRef = useRef(null);
   const titleRef = useRef(null);
-  const cardRefElements = useRef(new Array());
+  const cardRefElements = useRef<(HTMLDivElement | null)[]> ([]);
   useEffect(()=> {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: qualificationSectionRef.current,
         start: "top 80%",
-        end: "bottom 40%",
-        toggleActions: "play none none reset",
+        end:"center center",
+        toggleActions: "play none none res",
         scrub: true,
         markers: true,
       }
     });
     tl.fromTo(
       titleRef.current,
-      {x: -150, opacity: 0},
-      {x: 0, opacity: 1, duration: 0.6, ease: "power2.out"}
+      {x: -300, opacity: 0},
+      {x: 0, opacity: 1, duration: 1, ease: "power2.out"}
     )
+    cardRefElements.current.forEach((card)=> {
+      if(card){
+        
+        tl.fromTo(
+          card,
+          {x: -150, opacity: 0},
+          {x:0, opacity: 1, duration: 0.8, ease: "power2.out"},
+        )
+      }
+    })
   })
   return (
     <section ref={qualificationSectionRef} className='py-48 px-[12vw] 2xl:[16vw] flex flex-col gap-y-16 justify-center items-center w-full min-h-screen '>
@@ -36,6 +46,7 @@ const QualificationSection = () => {
         {qualificationData.map((item, index)=> (
             <CardQualification 
               key={index} id={item.id} school= {item.school} degree={item.degree} date={item.date} 
+              ref={(element)=> {cardRefElements.current[index] = element;}}
             />
         ))}
       </div>
