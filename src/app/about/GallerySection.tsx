d0f5991be5b9d4b86a782829a8366c55eac89a36
raiasim93 <asimrai89@gmail.com>
2025-04-05 20:51:51 +1100

    Complete bentor grid and add hover animations

--- src/app/about/GallerySection.tsx
+++ src/app/about/GallerySection.tsx
@@ -22,30 +22,34 @@ const GallerySection = () => {
             <div className='w-3/4 lg:w-1/2 text-3xl lg:text-6xl font-extrabold '> Running on Passion - whether it is Apps or Football</div>
             <div className='w-full text-xl lg:text-3xl font-semibold'> My life in Bento and Pics </div>
         </div>
-        <div className='w-full py-4 lg:py-12 min-h-screen grid grid-cols-12 grid-rows-6 gap-4'> 
-            <div className='col-span-3 row-span-4  relative'> 
-                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect1.jpg' />
+        <div className='w-full py-4 lg:py-12 min-h-screen  gap-5
+                             grid grid-cols-2
+                              lg:grid-cols-12 lg:grid-rows-6 
+                              md:grid-cols-6  md:grid-rows-12
+                        '> 
+            <div className='lg:col-span-3 lg:row-span-4 lg:order-1 md:col-span-2 md:row-span-6 md:order-1 relative'> 
+                <Image className='rounded-2xl object-cover duration-200 transition-all  cursor-pointer hover:shadow-2xl hover:shadow-lime-400 hover:scale-105 hover:border-white hover:border-2' fill alt='image' src='/rect1.jpg' />
             </div>
-            <div className='col-span-3 row-span-2 relative'> 
-                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/wide1.jpg' />
+            <div className='lg:col-span-3 lg:row-span-2 lg:order-2 md:col-span-2 md:row-span-3 md:order-2 relative'> 
+                <Image className='rounded-2xl object-cover duration-200 transition-all  cursor-pointer hover:shadow-2xl hover:shadow-lime-400 hover:scale-105 hover:border-white hover:border-2' fill alt='image' src='/wide1.jpg' />
              </div>
-            <div className='col-span-3 row-span-4 relative'> 
-                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect2.jpg' />
+            <div className='lg:col-span-3 lg:row-span-4 lg:order-3 md:col-span-2 md:row-span-6 md:order-3 relative'> 
+                <Image className='rounded-2xl object-cover duration-200 transition-all  cursor-pointer hover:shadow-2xl hover:shadow-lime-400 hover:scale-105 hover:border-white hover:border-2' fill alt='image' src='/rect2.jpg' />
              </div>
-            <div className='col-span-3 row-span-2 relative'> 
-                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/wide2.jpg' />
+            <div className='lg:col-span-3 lg:row-span-2 lg:order-4 md:col-span-2 md:row-span-3 md:order-4 relative'> 
+                <Image className='rounded-2xl object-cover duration-200 transition-all  cursor-pointer hover:shadow-2xl hover:shadow-lime-400 hover:scale-105 hover:border-white hover:border-2' fill alt='image' src='/wide2.jpg' />
              </div>
-            <div className='col-span-3 row-span-4 relative'> 
-                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect3.jpg' />
+            <div className='lg:col-span-3 lg:row-span-4  lg:order-5 md:col-span-2 md:row-span-6 md:order-5 relative'> 
+                <Image className='rounded-2xl object-cover duration-200 transition-all  cursor-pointer hover:shadow-2xl hover:shadow-lime-400 hover:scale-105 hover:border-white hover:border-2' fill alt='image' src='/rect3.jpg' />
              </div>
-            <div className='col-span-3 row-span-4 relative'> 
-                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect4.jpg' />
+            <div className='lg:col-span-3 lg:row-span-4 lg:order-6 md:col-span-2 md:row-span-6 md:order-6 relative'> 
+                <Image className='rounded-2xl object-cover duration-200 transition-all  cursor-pointer hover:shadow-2xl hover:shadow-lime-400 hover:scale-105 hover:border-white hover:border-2' fill alt='image' src='/rect4.jpg' />
              </div>
-            <div className='col-span-3 row-span-2 relative'> 
-                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/wide3.jpg' />
+            <div className='lg:col-span-3 lg:row-span-2 lg:order-7 md:col-span-2 md:row-span-3 md:order-7 relative'> 
+                <Image className='rounded-2xl object-cover duration-200 transition-all  cursor-pointer hover:shadow-2xl hover:shadow-lime-400 hover:scale-105 hover:border-white hover:border-2' fill alt='image' src='/wide3.jpg' />
              </div>
-            <div className='col-span-3 row-span-2 relative'> 
-                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect8.jpg' />
+            <div className='lg:col-span-3 lg:row-span-2 lg:order-8 md:col-span-2 md:row-span-3 md:order-7 relative'> 
+                <Image className='rounded-2xl object-cover duration-200 transition-all  cursor-pointer hover:shadow-2xl hover:shadow-lime-400 hover:scale-105 hover:border-white hover:border-2' fill alt='image' src='/wide4.jpg' />
              </div>
          
         </div>
