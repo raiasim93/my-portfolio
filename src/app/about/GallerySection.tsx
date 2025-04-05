97a4d507b75d01a2d16cbfa90373588e1b862ac5
raiasim93 <asimrai89@gmail.com>
2025-04-05 17:52:44 +1100

    Create a bento grid for screens for large screens and work on making it responsive

--- src/app/about/GallerySection.tsx
+++ src/app/about/GallerySection.tsx
@@ -17,14 +17,37 @@ const medias = [
 
 const GallerySection = () => {
   return (
-    <section className='py-[12vh] px-[6vw] 2xl:px-[16vw] min-h-screen bg-slate-900 rounded-2xl'>
-        <div className='columns-1 sm:columns-2 lg:columns-3 py-10 lg:py-20 gap-4'>
-            {medias.map((media, index)=>(
-                <div key={index} className='mb-4 break-inside-avoid'>
-                    <Image alt={`Image ${index}`} width={300} height={300} src={media}
-                           className='w-full object-cover rounded-xl'/>
-                </div>
-            ) )}
+    <section className='py-[12vh] px-[6vw] 2xl:px-[16vw]  bg-zinc-900 rounded-2xl'>
+        <div className='w-full flex justify-center flex-col gap-y-4 lg:gap-y-8 lg:justify-between mb-8 lg:mb-16'>
+            <div className='w-3/4 lg:w-1/2 text-3xl lg:text-6xl font-extrabold '> Running on Passion - whether it is Apps or Football</div>
+            <div className='w-full text-xl lg:text-3xl font-semibold'> My life in Bento and Pics </div>
+        </div>
+        <div className='w-full py-4 lg:py-12 min-h-screen grid grid-cols-12 grid-rows-6 gap-4'> 
+            <div className='col-span-3 row-span-4  relative'> 
+                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect1.jpg' />
+            </div>
+            <div className='col-span-3 row-span-2 relative'> 
+                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/wide1.jpg' />
+             </div>
+            <div className='col-span-3 row-span-4 relative'> 
+                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect2.jpg' />
+             </div>
+            <div className='col-span-3 row-span-2 relative'> 
+                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/wide2.jpg' />
+             </div>
+            <div className='col-span-3 row-span-4 relative'> 
+                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect3.jpg' />
+             </div>
+            <div className='col-span-3 row-span-4 relative'> 
+                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect4.jpg' />
+             </div>
+            <div className='col-span-3 row-span-2 relative'> 
+                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/wide3.jpg' />
+             </div>
+            <div className='col-span-3 row-span-2 relative'> 
+                <Image className='w-full rounded-2xl object-cover' fill alt='image' src='/rect8.jpg' />
+             </div>
+         
         </div>
     </section>
   )
