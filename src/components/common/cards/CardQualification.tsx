f684b30ace1b7b4dc995927c7609749919117b13
raiasim93 <asimrai89@gmail.com>
2025-04-10 16:30:32 +1000

    increase width of qualification card

--- src/components/common/cards/CardQualification.tsx
+++ src/components/common/cards/CardQualification.tsx
@@ -12,10 +12,10 @@ interface CardQualificationProps{
 const CardQualification  = React.forwardRef<HTMLDivElement, CardQualificationProps> (({school, degree, date, id}, ref) => {
     return (
         <div ref={ref}
-             key={id} className='w-[60%] lg:w-[40%] border-4 rounded-2xl p-8 min-h-[288px] border-l-4 border-l-lime-500 
+             key={id} className='w-[80%] lg:w-[40%] border-4 rounded-2xl p-4 min-h-[288px] border-l-4 border-l-lime-500 
                                 border-t-lime-500 flex flex-col gap-y-4 justify-center items-center
-                                 transition-all transform duration-300
-                                hover:scale-95 hover:bg-zinc-900 cursor-pointer'>
+                                 transition-al hover:scale-110 duration-300
+                                 hover:bg-zinc-800 cursor-pointer'>
             <div className='text-2xl text-center font-semibold flex flex-col gap-y-8 lg:text-3xl  items-center gap-x-4'>
                 <FaUniversity className='text-2xl lg:text-3xl' />
                 <span> {degree} </span>
