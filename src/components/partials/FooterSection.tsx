b7db4375b4ca42a2ca80be1db16bd52821b35ddb
raiasim93 <asimrai89@gmail.com>
2025-04-17 18:24:12 +1000

    Feature: Add FooterSection

---
+++ src/components/partials/FooterSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterSection = () => {
+  return (
+    <section className='py-8 px-[6vw] 2xl:px-[16vw] flex min-h-[50vh]'>
+        
+    </section>
+  )
+}
+
+export default FooterSection
