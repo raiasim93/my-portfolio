3a964a1b4125d981115fa57d14881214c3e8ef33
raiasim93 <asimrai89@gmail.com>
2025-04-23 18:16:20 +1000

    Improvement: Fix animation of cards from px to percent for smooth transition

--- src/app/experience/QualificationSection.tsx
+++ src/app/experience/QualificationSection.tsx
@@ -22,8 +22,8 @@ const QualificationSection = () => {
     });
     tl.fromTo(
       titleRef.current,
-      {x: -300, opacity: 0},
-      {x: 0, opacity: 1, duration: 1, ease: "power2.out"}
+      {xPercent: -100, opacity: 0},
+      {xPercent: 0, opacity: 1, duration: 1, ease: "power2.out"}
     )
     cardRefElements.current.forEach((card)=> {
       if(card){
@@ -38,7 +38,7 @@ const QualificationSection = () => {
   })
   return (
     <section ref={qualificationSectionRef} className='bg-black py-12 px-[6vw] 2xl:[16vw] flex flex-col gap-y-16 justify-center items-center w-full min-h-screen '>
-      <div ref={titleRef} className='text-4xl lg:text-8xl text-center font-semibold'>
+      <div ref={titleRef} className='text-4xl lg:text-6xl text-center font-semibold'>
          Qualifications over the years
       </div>
       <div className='flex flex-col gap-y-8 justify-center items-center h-full w-full'>
