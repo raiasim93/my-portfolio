a3c0d09aa59defc2c153b272f9eb73d846164960
raiasim93 <asimrai89@gmail.com>
2025-04-29 23:27:59 +1000

    Hotfix: Change the state when anything is clicked on the sidebar

--- src/components/partials/Sidebar.tsx
+++ src/components/partials/Sidebar.tsx
@@ -19,6 +19,7 @@ const Sidebar = ({ setSidebarOpen }: { setSidebarOpen: (open: boolean) => void }
           {["About", "Experience", "Resume"].map((item, index) => (
             <Link
               key={index}
+              onClick={()=> setSidebarOpen(false)}
               href={`/${item.toLowerCase()}`}
               className=' relative hover:text-zinc-400 hover:scale-105 transition-all duration-200 border-b border-white hover:border-lime-500 pb-2' >
               {item}
