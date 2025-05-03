79f92914cf5465aac4bf07bb537cef481b80e24d
raiasim93 <asimrai89@gmail.com>
2025-05-03 15:29:58 +1000

    Feature: Add alert message for projects route

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import { useEffect } from 'react'
 import React from 'react'
 import ProjectSection from './ProjectSection'
 
 const page = () => {
+  useEffect(()=> {
+    alert("Full Functionality is still a work in progress. Apologies AHhaha")
+  }, []);
   return (
     <>
     <ProjectSection/>
