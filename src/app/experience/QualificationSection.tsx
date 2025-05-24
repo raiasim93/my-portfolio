f840edbdd82dbb39461d82b838668890959f2292
raiasim93 <asimrai89@gmail.com>
2025-05-24 14:06:29 +1000

    Feature: Add accordion for education

--- src/app/experience/QualificationSection.tsx
+++ src/app/experience/QualificationSection.tsx
@@ -4,47 +4,55 @@ import { FaChevronDown } from "react-icons/fa";
 import qualificationData from '@/components/common/data/qualificationData';
 
 const QualificationSection = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [contentHeight, setContentHeight]= useState('0px');
-  const contentRef = useRef<HTMLDivElement | null>(null);
-  const description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio hic rem earum nisi beatae doloribus labore assumenda pariatur aut excepturi repellendus dolore eligendi debitis eveniet, voluptates, consectetur obcaecati eius mollitia est veritatis nesciunt.";
-  useEffect(()=> {
-    if(isOpen){
-        setContentHeight(`${contentRef.current?.scrollHeight}px`)
-    } else {
-      setContentHeight('0px');
-    }
-  }, [isOpen]);
+  const [openCards, setOpenCards] = useState<boolean[]>(() =>
+    new Array(qualificationData.length).fill(false));
+  const contentRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const handleClick = (index: number) => {
+    setOpenCards((prev) => {
+      const updatedCards = [...prev];
+      updatedCards[index] = !updatedCards[index];
+      return updatedCards;
+    });
+    console.log(`Card at index ${index} has value of ${openCards[index]} `)
+    console.log(`Card clicked at index: ${index}`);
+  }
   return (
-    <section className='py-16 px-[6vw] 2xl:px-[16vw] bg-zinc-950 flex  flex-col gap-y-4'>
+    <section className='py-16 px-[6vw] 2xl:px-[16vw] bg-zinc-950 flex  flex-col gap-y-8'>
       <div className='text-4xl'>
-        What and Where... 
+        What and Where...
       </div>
-      {qualificationData.map((item, index)=> (
-          <div key={index} className='bg-zinc-600 hover:bg-zinc-500 hover:duration-200 flex flex-col gap-y-4 p-4 rounded-xl cursor-pointer' 
-          onClick={()=> {setIsOpen(!isOpen); console.log(isOpen)}}>
-       <div className='flex flex-col-reverse lg:flex-row justify-between items-center'>
-         <div className='w-full text-xl'> {item.date} </div>
-         <div className='w-full flex items-center gap-x-4'>
-           <div className='w-full flex flex-col items-start lg:items-end'>
-             <div className='text-2xl'> {item.degree}</div>
-             <div className='text-gray-400'> {item.school}</div>
-           </div>
-           <div>
-             <FaChevronDown className={`${isOpen ? 'rotate-180 ease-in-out duration-300 transition-all': ''}`} />
-           </div>
-         </div>
-       </div>
-       {isOpen?  <div className='h-[2px] w-full bg-black transition-all duration-300 ease-in-out' /> : ""}
- 
-       <div ref={contentRef} 
-             style={{height: contentHeight}}
-            className={`overflow-hidden transition-all duration-300 ease-in-out}`}>
-          {description}
-       </div>
-     </div>
-      ) )}
-      
+      {qualificationData.map((item, index) => {
+        const expandedHeight = contentRefs.current[index]?.scrollHeight ?? 0;
+        return (
+          <div
+            onClick={() => handleClick(index)}
+            key={index} className='bg-zinc-600 hover:bg-zinc-500 hover:duration-200 flex flex-col gap-y-4 p-4 rounded-xl cursor-pointer'>
+            <div className='flex flex-col-reverse lg:flex-row justify-between items-center'>
+              <div className='w-full text-xl'> {item.date} </div>
+              <div className='w-full flex items-center gap-x-4'>
+                <div className='w-full flex flex-col items-start lg:items-end'>
+                  <div className='text-2xl'> {item.degree}</div>
+                  <div className='text-gray-400'> {item.school}</div>
+                </div>
+                <div>
+                  <FaChevronDown className={`transition-all duration-300 ease-in-out${openCards[index]? 'rotate-180': ''}`} />
+                </div>
+              </div>
+            </div>
+            {openCards[index] && (<div className= {`h-[2px] w-full bg-black transition-all duration-300 ease-in-out ${openCards[index]? 'opacity-100': 'opacity-0'}`}/>)}
+
+            <div ref={el => (contentRefs.current[index] = el)}
+              style={{
+                maxHeight: openCards[index] ? `${expandedHeight}px` : '0px'
+              }}
+              className={`overflow-hidden transition-all duration-300 ease-in-out}`}>
+              {item.description}
+            </div>
+          </div>
+        )
+
+      })}
+
     </section>
   )
 }
