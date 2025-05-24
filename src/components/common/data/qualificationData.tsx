16d0ed458b605154d35a4e45aee0814a5e25125f
raiasim93 <asimrai89@gmail.com>
2025-05-24 14:07:15 +1000

    Feature: Add description

--- src/components/common/data/qualificationData.tsx
+++ src/components/common/data/qualificationData.tsx
@@ -3,13 +3,15 @@ const qualificationData = [
         id: "1",
         school: "Federation University",
         degree: "Bachelor of Information Technology",
-        date: "2020 - 2024"
+        date: "2020 - 2024",
+        description: "Graduated with core knowledge in Networking, Web Development, and Cloud Computing. Strong foundation in modern IT practices and software development."
     },
     {
         id: "2",
         school: "National College of Computer Studies",
         degree: "High School Diploma",
-        date: "2017 - 2019"
+        date: "2017 - 2019",
+        description: "Completed higher secondary education with a focus on Computer Science and Business Mathematics, developing analytical and technical problem-solving skills."
     },
 ]
 
