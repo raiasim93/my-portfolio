aac80171d2a5443708989419cf9c92b97fbf6ed5
raiasim93 <asimrai89@gmail.com>
2025-06-12 11:39:25 +1000

    Refactor: Wrap next Image component in a secured div for better control

--- src/app/about/AboutSection.tsx
+++ src/app/about/AboutSection.tsx
@@ -45,7 +45,7 @@ const AboutSection = () => {
   return (
     <section ref={sectionRef} className='bg-black text-white py-12 px-[6vw] 2xl:px-[16vw] w-full flex flex-col lg:flex-row min-h-[100vh] justify-center gap-y-16 lg:gap-y-0 '>
       <div ref={aboutIntroRef} className='w-full lg:w-1/3 flex flex-col gap-y-4 lg:gap-y-8 justify-center lg:justify-start '>
-        <div className='text-2xl lg:text-4xl font-semibold text-center lg:text-start'> Hey! I&apos;m <span className='text-lime-400'> Asim. </span> </div>
+        <div className='text-3xl lg:text-4xl font-semibold text-center lg:text-start'> Hey! I&apos;m <span className='text-lime-400'> Asim. </span> </div>
         <div className='text-md lg:text-xl font-medium tracking-wide text-center lg:text-start w-2/3 lg:w-full mx-auto'>
           I&apos;m a Sydney-based Frontend Developer who loves designing and developing impactful brands as well as
           building innovative applications with strong emphasis on <span className='text-lime-400 text-2xl'> Product Experience.</span>
@@ -53,13 +53,15 @@ const AboutSection = () => {
       </div>
       
       <div ref={aboutImageRef} className='w-full lg:w-1/3 flex justify-center items-center '>
+        <div className='h-fit w-fit flex items-center justify-center'>
         <Image
-          className='aspect-square w-4/7 lg:w-3/4 object-cover border-2 border-white rounded-2xl'
+          className='aspect-square w-3/7 lg:w-3/4 object-cover border-2 border-white rounded-2xl'
           src='/aboutHero.jpeg'
           alt='Main Image of Hero Section'
           width={400}
           height={400}
         />
+        </div>
       </div>
       
       <div ref={aboutOutroRef} className='w-full lg:w-1/3 flex flex-col items-center gap-y-4 lg:gap-y-2 lg:justify-end lg:items-end '>
