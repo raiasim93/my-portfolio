3cb4e65ed0f4d9b19a49423d29318a727413f14a
raiasim93 <asimrai89@gmail.com>
2025-06-14 15:15:58 +1000

    Fix: Fix sizings and divider color

--- src/app/experience/QualificationSection.tsx
+++ src/app/experience/QualificationSection.tsx
@@ -26,9 +26,9 @@ const QualificationSection = () => {
         return (
           <div
             onClick={() => handleClick(index)}
-            key={index} className='bg-zinc-800 hover:bg-zinc-700 hover:duration-200 flex flex-col gap-y-4 p-4 rounded-xl cursor-pointer'>
-            <div className='flex flex-col-reverse lg:flex-row justify-between items-center'>
-              <div className='w-full text-xl'> {item.date} </div>
+            key={index} className={`bg-zinc-800 hover:bg-zinc-700 hover:duration-200 flex flex-col gap-y-4 p-4 rounded-xl cursor-pointer`}>
+            <div className={`flex flex-col-reverse lg:flex-row justify-between items-center` }>
+              <div className='w-full text-2xl'> {item.date} </div>
               <div className='w-full flex items-center gap-x-4'>
                 <div className='w-full flex flex-col items-start lg:items-end'>
                   <div className='text-2xl'> {item.degree}</div>
@@ -39,7 +39,7 @@ const QualificationSection = () => {
                 </div>
               </div>
             </div>
-            {openCards[index] && (<div className= {`h-[2px] w-full bg-black transition-all duration-300 ease-in-out ${openCards[index]? 'opacity-100': 'opacity-0'}`}/>)}
+            {openCards[index] && (<div className= {`h-[2px] w-full bg-white transition-all duration-300 ease-in-out ${openCards[index]? 'opacity-100': 'opacity-0'}`}/>)}
 
             <div ref={el => {contentRefs.current[index] = el}}
               style={{
