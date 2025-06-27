75094a5c8aaea689f0fccf9bd3505d2cce76ce7e
raiasim93 <asimrai89@gmail.com>
2025-06-27 10:33:30 +1000

    Improvement: Set height using offsetHeight

--- src/app/about/ToolsSection.tsx
+++ src/app/about/ToolsSection.tsx
@@ -1,12 +1,18 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import toolsData from "@/components/common/data/toolsData";
 import CardTools from "@/components/common/cards/CardTools";
 import { FaArrowDown} from "react-icons/fa";
 
 const ToolsSection = () => {
   const [showAll, setShowAll] = useState(false);
-
+  const [height, setHeight] = useState(0);
+  const toolDivRef = useRef<HTMLDivElement | null>(null);
+  useEffect(()=> {
+    if(toolDivRef.current){
+      setHeight(toolDivRef.current.offsetHeight);
+    }
+  })
   return (
     <section className="bg-black w-full py-16 px-[6vw] 2xl:px-[16vw] flex flex-col gap-y-8 ">
       <div className="flex lg:justify-center text-3xl font-semibold lg:text-5xl">
@@ -18,8 +24,9 @@ const ToolsSection = () => {
 
       {/* Tools Grid with Smooth Expansion */}
       <div
-        className={`tool-scrollbar grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 lg:overflow-hidden transition-all duration-500 overflow-y-auto
-                    ${showAll ? "max-h-[2000px]": "max-h-[200px]"}
+        ref={toolDivRef}
+        className={`tool-scrollbar grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8  lg:overflow-hidden transition-all duration-500 overflow-y-auto
+                    ${showAll ? {height}: "max-h-[200px]"}
         }`}
       >
         {toolsData.map((item, index) => (
@@ -27,7 +34,7 @@ const ToolsSection = () => {
         ))}
       </div>
       {/* Button */}
-      {/* <div className="mt-4 text-2xl flex justify-center w-full">
+      <div className="mt-4 text-2xl flex justify-center w-full">
         <button
           onClick={() => setShowAll(!showAll)}
           className="flex items-center gap-x-4 cursor-pointer text-lime-400 transition-transform duration-300"
@@ -37,7 +44,7 @@ const ToolsSection = () => {
             <FaArrowDown />
           </span>
         </button>
-      </div> */}
+      </div>
     </section>
   );
 };
