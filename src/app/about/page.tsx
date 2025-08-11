edaf83572db66d38195e6338349bbb75511ff4a4
raiasim93 <asimrai89@gmail.com>
2025-08-11 17:19:11 +1000

    Chore:

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutSection from './AboutSection'
 import GallerySection from './GallerySection';
+import VideoSection from './VideoSection';
 
 const page = () => {
   return (
     <>
         <AboutSection />
         <GallerySection/>
+        <VideoSection />
     </>
   )
 }
