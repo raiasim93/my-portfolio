7dc87b3464790f9ce3087661710ad0c2527b2d8a
raiasim93 <asimrai89@gmail.com>
2025-08-14 21:09:44 +1000

    Chores: Remove unused component

--- src/app/about/VideoSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const VideoSection = () => {
-  return (
-    <div>VideoSection</div>
-  )
-}
-
-export default VideoSection
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import AboutSection from './AboutSection'
 import GallerySection from './GallerySection';
-import VideoSection from './VideoSection';
 
 const page = () => {
   return (
     <>
         <AboutSection />
         <GallerySection/>
-        <VideoSection />
+  
     </>
   )
 }
