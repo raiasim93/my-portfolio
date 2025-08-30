51071448d083bc175fe8ef6afce01c70b580fa76
raiasim93 <asimrai89@gmail.com>
2025-08-30 21:05:18 +1000

    Chores

--- src/components/common/data/toolsData.tsx
+++ src/components/common/data/toolsData.tsx
@@ -4,7 +4,7 @@ const toolsData = [
         id: "1",
         toolName: "Next JS",
         image: <Image src={'/Next.js.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -13,7 +13,7 @@ const toolsData = [
         id: "2",
         toolName: "React",
         image: <Image src={'/React.png'}
-            className='aspect-square object-coverh-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-coverh-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -22,7 +22,7 @@ const toolsData = [
         id: "3",
         toolName: "Node.js",
         image: <Image src={'/Node.js.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -31,7 +31,7 @@ const toolsData = [
         id: "4",
         toolName: "TypeScript",
         image: <Image src={'/TypeScript.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -40,7 +40,7 @@ const toolsData = [
         id: "5",
         toolName: "Tailwind CSS",
         image: <Image src={'/Tailwind CSS.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -49,7 +49,7 @@ const toolsData = [
         id: "6",
         toolName: "JavaScript",
         image: <Image src={'/JavaScript.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -58,7 +58,7 @@ const toolsData = [
         id: "7",
         toolName: "React Bootstrap",
         image: <Image src={'/React Bootstrap.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -67,7 +67,7 @@ const toolsData = [
         id: "8",
         toolName: "GitHub",
         image: <Image src={'/GitHub.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -76,7 +76,7 @@ const toolsData = [
         id: "9",
         toolName: "Figma",
         image: <Image src={'/Figma.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -85,7 +85,7 @@ const toolsData = [
         id: "10",
         toolName: "HTML5",
         image: <Image src={'/HTML5.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -94,7 +94,7 @@ const toolsData = [
         id: "11",
         toolName: "CSS3",
         image: <Image src={'/CSS3.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -103,7 +103,7 @@ const toolsData = [
         id: "12",
         toolName: "Jira",
         image: <Image src={'/Jira.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='React Icon image'
             width={40}
             height={40} />
@@ -112,7 +112,7 @@ const toolsData = [
         id: "13",
         toolName: "gsap",
         image: <Image src={'/gsap.png'}
-            className='aspect-square object-cover h-16 w-16 lg:h-20 lg:w-20'
+            className='aspect-square object-cover h-16 w-16 lg:h-16 lg:w-16'
             alt='gsap Icon image'
             width={40}
             height={40} />
